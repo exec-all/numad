87e03e2781afc1df13c1219cd85186e8a449696c
Jay Coles <jay.coles@anchor.net.au>
2015-07-31 23:36:30 +1000

    Add more documentation and clean things up

--- numad/cmd.py
+++ numad/cmd.py
@@ -16,10 +16,14 @@ import sys
 
 
 @coroutine
-def display_loop(term):
+def display_loop(term, es_servers):
+	"""Starts and waits on inputs, slightly mangles data and sends them to be rendered
+	
+	This corresponds loosly with an event loop in game programming
+	"""
 	query = ""
 	# avoid recreating this all the time due to implicit dns lookups in creation
-	es = Elasticsearch(ES_SERVERS)
+	es = Elasticsearch(es_servers)
 
 	loop = asyncio.get_event_loop()
 
@@ -32,6 +36,10 @@ def display_loop(term):
 		done, pending = yield from asyncio.wait(jobs)
 		jobs = []
 
+
+		# this needs to come before input handling as it may try
+		# and cancel a compleated job before its processed. as resuls
+		# take a bit to come back its better to just briefly display them
 		if search in done:
 			results = search.result()
 			render_search_results(term, results)
@@ -39,11 +47,17 @@ def display_loop(term):
 		if input in done:
 			query = input.result()
 			render_query_field(term, query)
+			# we wrap this is a task for the membership query above
+			# if we just pass in a corutine, asyncio.wait takes it upon
+			# itself to wrap that in a Task and the comparison will always
+			# fail
 			input = Task(input_loop(loop, term, query))
 			jobs.append(input)
 
-			# we wont be using the results anymore so just discard the worker
+			# ES does not return results to us for short queries, so just short
+			# circuit them and avoid network traffic
 			if len(query) > 1:
+				# we wont be using the results anymore so just discard the worker
 				search.cancel()
 				search = Task(search_loop(loop, es, query))
 				jobs.append(search)
@@ -53,6 +67,8 @@ def display_loop(term):
 
 		sys.stdout.flush()
 
+		# Make sure we resubmit any jobs that have not yet been compleated
+		# otherwise we leak them and things go into limbo
 		jobs += pending
 
 
@@ -63,11 +79,14 @@ def main(argv=sys.argv[1:]):
 	
 	options = args.parse_args(argv)
 	
+	# post process argparsing results
 	options.servers = [x.strip() for x in options.servers.split(",")]
 	
 	loop = asyncio.get_event_loop()
 	t = Terminal()
 	
+	# Do this as a fullscreen app where possible to avoid poluting
+	# terminal and screwing up cursor position
 	with t.fullscreen():
 		with t.cbreak():
 			# this blank print statment forces blessings to go to 
@@ -80,6 +99,10 @@ def main(argv=sys.argv[1:]):
 			sys.stdout.flush()
 	
 			try:
-				loop.run_until_complete(display_loop(t))
+				loop.run_until_complete(display_loop(t, options.servers))
 			except KeyboardInterrupt:
+				# We want to catch this as its out primary exit method. if
+				# uncaught we end up displaying a interleaved (with old output)
+				# stack trace that would incorectly lead users to assume they 
+				# crashed the program
 				print("Exiting")
