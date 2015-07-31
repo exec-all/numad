dbd4ebea92cb59d896fa4635b5f21d4362ed0d46
Jay Coles <jay.coles@anchor.net.au>
2015-07-31 22:26:46 +1000

    Add in project metadata for introspection

--- numad/__init__.py
+++ numad/__init__.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 """umad: pull results from umad"""
 
+__author__ = "Jay Coles"
+__version__ = "0.1"
+__email__ = "code@pocketnix.org"
+__maintainer_email__ = "jayc@anchor.net.au"
+__license__ = "BSD (2 Clause)"
+__url__ = "https://github.com/exec-all/numad"
+
+
 from asyncio import coroutine, Task
 from aioes import Elasticsearch
 from blessed import Terminal
